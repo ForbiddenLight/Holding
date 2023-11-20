def6fbbae67bdea9e31413084800325d52a2e1c8
ForbiddenLight <136534710+ForbiddenLight@users.noreply.github.com>
2023-11-19 23:29:23 -0500

    Update AA Gun auto things.lua

--- Naval Warfare/AA Gun auto things.lua
+++ Naval Warfare/AA Gun auto things.lua
@@ -3,7 +3,9 @@ local Player = Players.LocalPlayer
 local RStorage = game:GetService("ReplicatedStorage")
 local PlayerInput = game:GetService("UserInputService")
 
-_G.Type = "Planes"
+_G.EnableDebug = true
+
+getgenv().ShootAtType = "Planes"
 if _G.TypeChanger then 
 	_G.TypeChanger:Disconnect()
 end
@@ -12,12 +14,13 @@ _G.TypeChanger = PlayerInput.InputBegan:Connect(function(Key, Process)
 	if Key.KeyCode == Enum.KeyCode.L and not Process then 
 		local Type = tostring(_G.Type)
 		
-		if Type == "Planes" then 
-			Type = "Players"
-		elseif Type == "Players" then 
-			Type = "Planes"
+		if ShootAtType == "Planes" then 
+			ShootAtType = "Players"
+		elseif ShootAtType == "Players" then 
+			ShootAtType = "Planes"
 		end
-		_G.Type = Type
+		_G.Type = ShootAtType
+		print(ShootAtType)
 	end
 end)
 
@@ -27,7 +30,7 @@ local function FetchClosest()
 	if Player.Character and Player.Character.PrimaryPart then
 		local PlayerTeam = tostring(Player.Team.Name)
 		
-		if Type == "Planes" then 
+		if ShootAtType == "Planes" then 
 			local ClosestPlane --Instance
 			local Magnitude = math.huge
 			for _, v in pairs(workspace:GetChildren()) do 
@@ -46,8 +49,12 @@ local function FetchClosest()
 					end
 				end
 			end
-			return ClosestPlane.MainBody
-		elseif Type == "Players" then 
+			if ClosestPlane ~= nil then 
+				return ClosestPlane:WaitForChild("MainBody", 2)
+			elseif ClosestPlane == nil then 
+				return false
+			end
+		elseif ShootAtType == "Players" then 
 			local ClosestPlayer --Instance
 			local Magnitude = math.huge
 			for _, v in pairs(Players:GetPlayers()) do 
@@ -67,7 +74,11 @@ local function FetchClosest()
 					end
 				end
 			end
-			return ClosestPlayer.PrimaryPart
+			if ClosestPlayer ~= nil then 
+				return ClosestPlayer:WaitForChild("Torso", 2)
+			elseif ClosestPlayer == nil then 
+				return false
+			end
 		end
 	end
 	return nil
@@ -83,18 +94,33 @@ MT.__namecall = newcclosure(function(Remote, ...)
     if Remote.Name == "Event" and Method == "FireServer" then
         if Args[1] ~= nil and Args[1] == "shoot" and Args[2][1] ~= nil and typeof(Args[2][1]) == "boolean" and Args[2][1] == true then 
 			_G.IsShooting = true
+			print("shoot")
 			coroutine.resume(coroutine.create(function()
 				while _G.IsShooting == true do 
 					local Closest = FetchClosest()
-					if Closest ~= nil then
-						GameEvent[Method]( Remote, "aim", {[1] = Closest.Position + (Closest.Velocity * (Player:GetNetworkPing() / 100 * 18.2) ) } )
+					if Closest == false or Closest == nil then
+						GameEvent[Method](Remote, unpack(Args))
+						print("failed, target not found.")
+						task.wait(0.1)
+					elseif Closest ~= nil then 
+						if ShootAtType == "Planes" then 
+							GameEvent[Method]( Remote, "aim", {[1] = Closest.Position + (Closest.Velocity * (Player:GetNetworkPing() / 100 * 21.2) ) } )
+							if _G.EnableDebug then
+								print("plane target")
+							end
+						elseif ShootAtType == "Players" then 
+							GameEvent[Method]( Remote, "aim", {[1] = Closest.Position + (Closest.Velocity * (Player:GetNetworkPing() / 100 * 0.21) ) } )
+							if _G.EnableDebug then
+								print("player target")
+							end
+						end
 						task.wait(0.1)
 					end
 				end
 			end))
-			return GameEvent[Method](Remote, "shoot", {[1] = true})
 		elseif Args[1] ~= nil and Args[1] == "shoot" and Args[2][1] ~= nil and typeof(Args[2][1]) == "boolean" and Args[2][1] == false then
 			_G.IsShooting = false		
+			print("stopped shoot")
 			return GameEvent[Method](Remote, "shoot", {[1] = false})
 		end
     end
